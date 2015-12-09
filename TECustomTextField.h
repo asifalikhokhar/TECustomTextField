d8f4b4243c1d19adeefd68e34fa2650399e72d4c
Asif Ali <asifalikhokhar@users.noreply.github.com>
2015-12-09 15:02:27 +0500

    Create TECustomTextField.h

---
+++ TECustomTextField.h
@@ -0,0 +1,19 @@
+//
+//  TECustomTextField.h
+//  CelebrityApp
+//
+//  Created by Asif Ali on 12/9/15.
+//  Copyright © 2015 v7iTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TECustomTextField : UITextField
+{
+    
+}
+
+@property (nonatomic, strong) IBInspectable UIColor *placeholderColor;
+@property (nonatomic, strong) IBInspectable UIImage *leftIcon;
+@property (nonatomic, strong) IBInspectable UIImage *rightIcon;
+@end
